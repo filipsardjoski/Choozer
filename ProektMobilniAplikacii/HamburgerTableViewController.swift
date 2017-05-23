db5ed0802e14015d66bfb74fd261f7a0534f853b
Ljubomir Jankovikj <ljubomirjankovikj@Ljubomirs-Mac.local>
2017-05-23 13:28:44 +0200

    Hamburger Class added

---
+++ ProektMobilniAplikacii/HamburgerTableViewController.swift
@@ -0,0 +1,186 @@
+//
+//  HamburgerTableViewController.swift
+//  Domashna SB
+//
+//  Created by NBO AIR on 5/2/17.
+//  Copyright © 2017 Ljubomir Jankovikj. All rights reserved.
+//
+
+import UIKit
+import MMDrawerController
+
+class HamburgerTableViewController: UITableViewController {
+
+    var Meni: [String] = ["Coin flip","Dice roll","Rock Paper Scissors","Yes/No","Pick from List"]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.backgroundColor = UIColor.black
+        self.tableView.separatorStyle = .none
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Meni.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        
+        //Staj slika za pozadina na edniot cell
+        //let pozadina = UIImageView(image: #imageLiteral(resourceName: "whiteDesign"))
+        cell.textLabel?.textColor = UIColor.white;
+       
+        // cell.selectedBackgroundView = pozadina
+        cell.textLabel?.text = Meni[indexPath.row]
+        cell.backgroundColor = .clear
+
+
+        return cell
+    }
+ 
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath)!
+        selectedCell.contentView.backgroundColor = UIColor.gray
+        
+        
+        switch (indexPath.row) {
+            
+        case 0:
+            
+            let centerViewController = self.storyboard?.instantiateViewController(withIdentifier: "CoinFlipViewController") as! CoinFlipViewController
+            let centerNavController = UINavigationController(rootViewController: centerViewController)
+            let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            appDelegate.centerContainer!.centerViewController = centerNavController
+            appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)
+            
+            break;
+            
+        case 1:
+            
+            let centerViewController = self.storyboard?.instantiateViewController(withIdentifier: "DiceRollViewController") as! DiceRollViewController
+            let centerNavController = UINavigationController(rootViewController: centerViewController)
+            let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            appDelegate.centerContainer!.centerViewController = centerNavController
+            appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)
+            
+            break;
+            
+            
+        case 2:
+            
+            let centerViewController = self.storyboard?.instantiateViewController(withIdentifier: "RockPaperScissorsViewController") as! RockPaperScissorsViewController
+            let centerNavController = UINavigationController(rootViewController: centerViewController)
+            let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            appDelegate.centerContainer!.centerViewController = centerNavController
+            appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)
+            
+            break;
+    
+        
+        case 3:
+            
+            let centerViewController = self.storyboard?.instantiateViewController(withIdentifier: "YesNoViewController") as! YesNoViewController
+            let centerNavController = UINavigationController(rootViewController: centerViewController)
+            let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            appDelegate.centerContainer!.centerViewController = centerNavController
+            appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)
+            
+            break;
+   
+            
+        case 4:
+            
+            let centerViewController = self.storyboard?.instantiateViewController(withIdentifier: "PickFromListViewController") as! PickFromListViewController
+            let centerNavController = UINavigationController(rootViewController: centerViewController)
+            let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            appDelegate.centerContainer!.centerViewController = centerNavController
+            appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)
+            
+            break;
+            
+
+
+        default:
+            print("test")
+        }
+        
+        
+        
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
