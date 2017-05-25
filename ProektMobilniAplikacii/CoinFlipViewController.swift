53d1e30a9ecb47274bdd027459f8e38a5b5e358b
Ljubomir Jankovikj <ljubomirjankovikj@Ljubomirs-Mac.local>
2017-05-25 03:55:01 +0200

    Coin Flip animation added, constraints should be fixed,

--- ProektMobilniAplikacii/CoinFlipViewController.swift
+++ ProektMobilniAplikacii/CoinFlipViewController.swift
@@ -19,6 +19,13 @@ class CoinFlipViewController: UIViewController {
 
     }
     
+    @IBOutlet weak var gifce: UIImageView!
+    @IBOutlet weak var heads: UIImageView!
+    @IBOutlet weak var tails: UIImageView!
+    
+    
+    
+    
     var coin : Int = 0 //0 - Heads | 1 - Tails
     
     @IBOutlet weak var CoinFlipLabel: UILabel!
@@ -26,14 +33,38 @@ class CoinFlipViewController: UIViewController {
         coin = Int(arc4random_uniform(2))
         if (coin == 0) { //Heads
             CoinFlipLabel.text = "Heads"
+            self.heads.isHidden=true
+            self.tails.isHidden=true
+            self.gifce.isHidden=false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                // do after 1.5s
+                self.gifce.isHidden=true
+                self.heads.isHidden=false
+                
+            }
+            
         } else {
             CoinFlipLabel.text = "Tails"
+            self.heads.isHidden=true
+            self.gifce.isHidden=false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                // do after 1.5s
+                self.gifce.isHidden=true
+                self.tails.isHidden=false
+                
+            }
+            
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.black
+        gifce.loadGif(name: "he")
+        gifce.isHidden=true
+        heads.isHidden=false
+        tails.isHidden=true
         // Do any additional setup after loading the view.
     }
 
