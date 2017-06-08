a7bd32eccdcbedc146128c1250dfc93d34c4539e
Filip Sardzoski <filipsardzoski@filips-macbook-air.local>
2017-06-08 21:46:29 +0200

    Add Dice Roll

--- ProektMobilniAplikacii/DiceRollViewController.swift
+++ ProektMobilniAplikacii/DiceRollViewController.swift
@@ -12,38 +12,96 @@ import MMDrawerController
 
 class DiceRollViewController: UIViewController {
     
+    @IBOutlet weak var segmentedPicker: UISegmentedControl!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var dice_2_1: UIImageView!
+    @IBOutlet weak var dice_2_2: UIImageView!
+    @IBOutlet weak var dice_1: UIImageView!
+    
+    var numberOfDice : Int = 1
+    
+    @IBAction func segmentedPickerPressed(_ sender: Any) {
+        numberOfDice = segmentedPicker.selectedSegmentIndex + 1
+        //RANDOMISE
+        if (numberOfDice == 1) { //1 Die
+            dice_2_1.isHidden = true
+            dice_2_2.isHidden = true
+            dice_1.isHidden = false
+            let randomNumber = Int(arc4random_uniform(6)) + 1
+            dice_1.image = UIImage (named: String("dice\(randomNumber)"))
+        } else { //2 Dice
+            dice_2_1.isHidden = false
+            dice_2_2.isHidden = false
+            dice_1.isHidden = true
+            var randomNumber = Int(arc4random_uniform(6)) + 1
+            dice_2_1.image = UIImage (named: String("dice\(randomNumber)"))
+            randomNumber = Int(arc4random_uniform(6)) + 1
+            dice_2_2.image = UIImage (named: String("dice\(randomNumber)"))
+        }
+    }
+    
+    @IBAction func rollDicePressed(_ sender: Any) {
+        if (numberOfDice == 1) { //1 Die
+            var randomNumber : Int = 0
+            for _ in 1...10 {
+                randomNumber = Int(arc4random_uniform(6)) + 1
+            }
+            resultLabel.text = "It's a \(randomNumber)"
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = Double.pi
+            rotationAnimation.duration = 0.2
+            self.dice_1.layer.add(rotationAnimation, forKey: nil)
+            
+            dice_1.image = UIImage (named: String("dice\(randomNumber)"))
+        } else { //2 Dice
+            var randomNumber : Int = 0
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = Double.pi
+            rotationAnimation.duration = 0.4
+            self.dice_2_1.layer.add(rotationAnimation, forKey: nil)
+            self.dice_2_2.layer.add(rotationAnimation, forKey: nil)
+            
+            for _ in 1...10 {
+                randomNumber = Int(arc4random_uniform(6)) + 1
+            }
+            var temp = randomNumber
+            dice_2_1.image = UIImage (named: String("dice\(randomNumber)"))
+            for _ in 1...10 {
+                randomNumber = Int(arc4random_uniform(6)) + 1
+            }
+            resultLabel.text = "It's a \(temp), and it's a \(randomNumber)"
+            dice_2_2.image = UIImage (named: String("dice\(randomNumber)"))
+        }
+    }
     
     @IBAction func HamButtonPressed(_ sender: Any) {
         let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
         
-        appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)    }
-    
-    @IBOutlet weak var DiceRollLabel: UILabel!
-    @IBAction func RollButtonIsPressed(_ sender: Any) {
-       let dice = Int(arc4random_uniform(6))
-        DiceRollLabel.text = String (dice + 1)
+        appDelegate.centerContainer!.toggle(MMDrawerSide.left, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title="Dice roll"
-        // Do any additional setup after loading the view.
+        self.title = "Dice Roll"
+        segmentedPicker.selectedSegmentIndex = 0
+        numberOfDice = 1
+        
+        dice_2_1.isHidden = true
+        dice_2_2.isHidden = true
+        dice_1.isHidden = false
+        let randomNumber = Int(arc4random_uniform(6)) + 1
+        dice_1.image = UIImage (named: String("dice\(randomNumber)"))
+        resultLabel.text = "It's a \(randomNumber)"
+        
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
